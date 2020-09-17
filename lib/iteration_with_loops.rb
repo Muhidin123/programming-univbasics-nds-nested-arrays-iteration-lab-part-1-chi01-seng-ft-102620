be8ce709477f123336d07debad2c96718083c375
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-17 16:53:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,13 @@
 def find_even_values(src)
-  src.select(&:even)
+  ix = 0 
+  while ix < src.count do
+    each_ix = 0
+    while each_ix < src[ix].count do
+      if src[ix][each_ix].even?
+        p src[ix][each_ix]
+      end
+      each_ix += 1
+    end
+    ix += 1
+  end
 end
